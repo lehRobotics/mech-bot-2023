53beb5d7983d9bce5454a45fefb5b92f7106be28
LowPolyCat <152674145+L0wPolyCat@users.noreply.github.com>
2024-09-05 18:40:45 -0600

    Update MechDrive

--- main/java/frc/robot/commands/MechDrive.java
+++ main/java/frc/robot/commands/MechDrive.java
@@ -19,17 +19,26 @@ public class MechDrive extends CommandBase {
 
     }
 
+    
+    double FRMgoal = 0.0;
+    double FLMgoal = 0.0;
+    double BRMgoal = 0.0;
+    double BRMgoal = 0.0;
     public void execute() {
-
         double driverAxisX = RobotContainer.CONTROLLER_JOYSTICK.getRawAxis(0);
         double driverAxisY = RobotContainer.CONTROLLER_JOYSTICK.getRawAxis(1);
         double driverAxisZ = RobotContainer.CONTROLLER_JOYSTICK.getRawAxis(2);
 
+        if ((driverAxisY+driverAxisX+driverAxisZ) > FRMgoal) {
+            System.out.print("Increase Speed Goal");
+        } else {
+            System.out.print("Decrease Speed Goal");
+        }
+        
         RobotContainer.DRIVE_TRAIN.FRMset((driverAxisY+driverAxisX+driverAxisZ)/Constants.robotConstants.MOVE_SPEED);
         RobotContainer.DRIVE_TRAIN.FLMset((driverAxisY-driverAxisX-driverAxisZ)/Constants.robotConstants.MOVE_SPEED);
         RobotContainer.DRIVE_TRAIN.BRMset((driverAxisY-driverAxisX+driverAxisZ)/Constants.robotConstants.MOVE_SPEED);
         RobotContainer.DRIVE_TRAIN.BLMset((driverAxisY+driverAxisX-driverAxisZ)/Constants.robotConstants.MOVE_SPEED);
-
     }
 
     public void end(boolean interrupted) {
