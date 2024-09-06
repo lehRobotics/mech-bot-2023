46c81d9213f7fb3a9f5801224dc3ea3e13afba21
LowPolyCat <152674145+L0wPolyCat@users.noreply.github.com>
2024-09-05 22:09:52 -0600

    Update ControllerInputs

--- main/java/frc/robot/subsystems/ControllerInputs.java
+++ main/java/frc/robot/subsystems/ControllerInputs.java
@@ -1,1 +1,12 @@
+package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
+
+public class ControllerInputs extends SubsystemBase{
+    public ControllerInputs() {}
+
+    public boolean TriggerIsDown() {
+      return false;
+    }
+}
